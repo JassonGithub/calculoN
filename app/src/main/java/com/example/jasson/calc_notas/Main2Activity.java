479cb77c07e649721bc9cdd0c8b60c44d96406cc
JassonGithub <jassonferfloz@gmail.com>
2017-10-05 08:01:36 -0500

    ertert

--- app/src/main/java/com/example/jasson/calc_notas/Main2Activity.java
+++ app/src/main/java/com/example/jasson/calc_notas/Main2Activity.java
@@ -20,6 +20,7 @@ public class Main2Activity extends AppCompatActivity {
                 Intent i = new Intent(Main2Activity.this, MainActivity.class);
                 startActivity(i);
                 finish();
+                
 
             }
         },2500);
